58006ea1d87cfe9a0275136d3613450fc11dc2f4
mb-michael <55842528+mb-michael@users.noreply.github.com>
2019-10-03 12:36:43 -0400

    Add Camps Controller, and configure GET response

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    //ControllerBase is specialized for APIs
+    public class CampsController : ControllerBase
+    {
+        //route + action = endpoint
+        [HttpGet]
+        public IActionResult GetCamps()
+        {
+            //successful response
+            return Ok(new { Moniker = "ATL2018", Name = "Atlanta Code Camp" });
+        }
+    }
+}
